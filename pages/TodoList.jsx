2ae77f1a8aa020653d994d5074778aaa6381cf80
whitebsm93 <whitebsm93@naver.com>
2022-08-16 17:55:13 +0900

    Layout, Header

---
+++ pages/TodoList.jsx
@@ -0,0 +1,17 @@
+import React, {useState} from "react";
+import Form from "../components/form/Form";
+import Header from "../components/header/Header";
+import Layout from "../components/layout/Layout";
+import List from "../components/list/List";
+
+const TodoList = () => {
+    
+
+    return (
+        <Layout>
+            <Header />
+        </Layout>
+    )
+}
+
+export default TodoList
---
+++ src/components/header/Header.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./style.css";
+
+const header = () => {
+    return (
+        <div className="header">
+            <div>My To-do List</div>
+            <div>Made by Me</div>
+        </div>
+    )
+}
+
+export default Header
