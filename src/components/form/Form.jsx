d5c819df7635ef5a1a8ca4e8191c7080fe95d27c
whitebsm93 <whitebsm93@naver.com>
2022-08-17 13:14:46 +0900

    기능 오류 수정

--- src/components/form/Form.jsx
+++ src/components/form/Form.jsx
@@ -3,24 +3,25 @@ import "./style.css";
 
 let count = 3
 
-const Form = ({ setTodos, todos}) => {
+const Form = ({todos, setTodos}) => {
     const initialState = {
         id: 0,
         title: "",
-        body: "",
+        details: "",
         isDone: false
     }
     
     const [todo, setTodo] = useState(initialState)
     const onChangeHandler = (event) => {
-        const { title, details } = event.target
-        setTodo({...todo, [title]:details})
+        const {name, value} = event.target
+        setTodo({...todo, [name]:value})
     }
 
     const onSubmitHandler = (event) => {
         event.preventDefault()
         if (todo.title.trim() === "" || todo.details.trim() === "") return;
-        setTodos([...todos, {...todo, id: count}])
+        console.log(todo.title, todo.details)
+        setTodos([...todos, {...todo, id:count}])
         setTodo(initialState)
         count++
     }
@@ -29,10 +30,10 @@ const Form = ({ setTodos, todos}) => {
         <form className="add-form" onSubmit={onSubmitHandler}>
             <div className="input-group">
                 <label className="input-group-text">Title</label>
-                <input type="text" aria-label={todo.title} className="form-control" placeholder="뭐해야 돼?" name='title' onChange={onChangeHandler} />
+                <input type="text" name='title' value={todo.title} className="form-control" placeholder="뭐해야 돼?" onChange={onChangeHandler} />
                 <label className="input-group-text">Details</label>
-                <input type="text" aria-label={todo.details} className="form-control" placeholder="자세한 내용은?" name='details' onChange={onChangeHandler} />
-                <button className="btn btn-primary" type="button" id="button-addon2">Add To-do</button>
+                <input type="text" name='details' value={todo.details} className="form-control" placeholder="자세한 내용은?" onChange={onChangeHandler} />
+                <button className="btn btn-primary" id="button-addon2">Add To-do</button>
             </div>
         </form>
     )
